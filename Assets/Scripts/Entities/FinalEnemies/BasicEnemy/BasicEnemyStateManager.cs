60b437dfcb13b5d4680f5c7451d9273d0cdce529
TdewT <szwej.jakub@gmail.com>
2025-01-10 21:28:14 +0100

    Moved all abstract code from `StateManager` into a final class
    Abstract classes will be added as needed in the future.

---
+++ Assets/Scripts/Entities/FinalEnemies/BasicEnemy/BasicEnemyStateManager.cs
@@ -0,0 +1,41 @@
+﻿using Enemies.Abstracts;
+using Entities.Interfaces;
+using Entities.States;
+using UnityEngine;
+
+namespace Entities.FinalEnemies.BasicEnemy
+{
+    public class BasicEnemyStateManager : MonoBehaviour, IEntityStateManager
+    {
+        // ReSharper disable once MemberCanBePrivate.Global
+        public IEntityState CurrentState { get; private set; }
+
+        private readonly IdleState idleState = new IdleState();
+        private readonly ChasingState chasingState = new ChasingState();
+        private readonly AttackState attackingState = new AttackState();
+        private readonly WanderingState wanderingState = new WanderingState();
+
+        public IMovement Movement { get; set; }
+        public AnimationController AnimationController { get; set; }
+
+        private void Start()
+        {
+            Movement = GetComponent<IMovement>();
+            AnimationController = GetComponent<AnimationController>();
+
+            TransitionToState(wanderingState);
+        }
+
+        private void Update()
+        {
+            CurrentState.UpdateState(this);
+        }
+
+        private void TransitionToState(IEntityState newState)
+        {
+            CurrentState?.ExitState(this);
+            CurrentState = newState;
+            CurrentState.EnterState(this);
+        }
+    }
+}
