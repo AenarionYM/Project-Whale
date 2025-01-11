9a59421b2c771d411ec4093f020c70c6a5a8dd41
TdewT <szwej.jakub@gmail.com>
2025-01-11 10:21:23 +0100

    Implemented `WanderingState`

--- Assets/Scripts/Entities/States/WanderingState.cs
+++ Assets/Scripts/Entities/States/WanderingState.cs
@@ -1,22 +1,48 @@
 ﻿using Entities.Interfaces;
+using Entities.States.Enums;
+using UnityEngine;
 
 namespace Entities.States
 {
     public class WanderingState : IEntityState
     {
+        private Vector2 initialPosition;
+        private Vector2 targetPosition;
+        
+        public MovementType MovementType => MovementType.Walk;
+        public Interactivity Interactivity => Interactivity.Active;
+
+        public WanderingState(Vector2 initialPosition)
+        {
+            this.initialPosition = initialPosition;
+        }
+
+
         public void EnterState(IEntityStateManager entity)
         {
-            throw new System.NotImplementedException();
+            SetNewTargetPosition();
+            entity.Movement.MoveTo(targetPosition);
         }
 
         public void UpdateState(IEntityStateManager entity)
         {
-            throw new System.NotImplementedException();
+            // Currently no per frame logic needed
         }
 
         public void ExitState(IEntityStateManager entity)
         {
-            throw new System.NotImplementedException();
+            // Currently no cleanup logic needed
+        }
+
+        private void SetNewTargetPosition()
+        {
+            Vector2 randomDirection = Random.insideUnitCircle * 10f;
+            targetPosition = initialPosition + randomDirection;
+        }
+
+        public void SetNewInitialPosition(Vector2 newInitialPosition)
+        {
+            initialPosition = newInitialPosition;
         }
     }
 }
