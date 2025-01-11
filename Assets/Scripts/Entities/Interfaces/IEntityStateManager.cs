314424a30e607b4074b12a0b6134a826721120f8
TdewT <szwej.jakub@gmail.com>
2025-01-11 09:55:52 +0100

    Added missing fields to the `IEntityStateManager`interface

--- Assets/Scripts/Entities/Interfaces/IEntityStateManager.cs
+++ Assets/Scripts/Entities/Interfaces/IEntityStateManager.cs
@@ -1,6 +1,12 @@
-﻿namespace Entities.Interfaces
+﻿using System;
+using Enemies.Abstracts;
+
+namespace Entities.Interfaces
 {
     public interface IEntityStateManager
-    {   
+    {
+        public IMovement Movement { get; set; }
+        public AnimationController AnimationController { get; set; }
+        public event Action<IEntityState> OnStateChanged;
     }
 }
